536a5f73c211e73c4e01eb7c8922e94e12669630
Laura Daniela Morales Gutierrez <laumoral@c2r12s1.42madrid.com>
2023-01-11 11:15:09 +0100

    isalpha completed

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,8 +6,26 @@
 /*   By: laumoral <laumoral@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 10:40:14 by laumoral          #+#    #+#             */
-/*   Updated: 2023/01/11 10:40:43 by laumoral         ###   ########.fr       */
+/*   Updated: 2023/01/11 11:04:10 by laumoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include <unistd.h>
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+/*int main(void)
+{
+    int c = 121;
+    int result = ft_isalpha(c);
 
+    if (result == 0)
+        write(1,"FALSE", 5);
+    else if (result == 1)
+        write(1, "TRUE", 4);
+    return (0);
+}*/
