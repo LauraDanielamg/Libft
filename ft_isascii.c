4bb12cd3c4d0472e97e11c4e4fe2bd74164e18d7
Laura Daniela Morales Gutierrez <laumoral@c2r12s1.42madrid.com>
2023-01-11 13:01:51 +0100

    isascii done

---
+++ ft_isascii.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laumoral <laumoral@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 12:52:09 by laumoral          #+#    #+#             */
+/*   Updated: 2023/01/11 12:55:50 by laumoral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	else
+		return (0);
+}
+/*int main(void)
+{
+    int c = 121;
+    int result = ft_isascii(c);
+    if (result == 0)
+        write(1,"FALSE", 5);
+    else if (result == 1)
+        write(1, "TRUE", 4);
+    return (0);
+}*/
