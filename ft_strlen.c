dff6abda495edc0ae64e199c318dee9a39cc640c
Laura Daniela Morales Gutierrez <laumoral@c2r12s1.42madrid.com>
2023-01-11 16:06:51 +0100

    ft_strlen.c with main

---
+++ ft_strlen.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laumoral <laumoral@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 16:01:40 by laumoral          #+#    #+#             */
+/*   Updated: 2023/01/11 16:06:18 by laumoral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/*#include <stdio.h>
+int main(void)
+{
+   const char *str = "Hola";
+   size_t result;
+   result = ft_strlen(str);
+   printf("El valor es %zu.\n", result);
+   return (0);
+}*/
