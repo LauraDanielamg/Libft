d2e12feeb012e9d2de0571373522f7a5326a2df5
Laura Daniela <Laura@Wanaldinos-MacBook-Pro.local>
2023-01-24 21:15:21 +0100

    ft_strncmp.c done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -20,25 +20,38 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	while ((i < n) && (*s1 != '\0') && (*s2 != '\0'))
 	{
-		if (s1[i] < s2[i])
-			return (-1);
-		else if (s1[i] > s1[i])
-			return (1);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (0);
+	if (i == n)
+		return (0);
+	else
+		return (s1[i] - s2[i]);
 }
 
 int main ()
 {
-    char *s1 = "Arbol";
-    char *s2 = "Artesania";
-    char *s3 = "Zorro";
+    char *s1 = "Bed";
+    char *s2 = "Bedroom";
+    char *s3 = "Door";
+    char *s4 = "";
+    char *s5 = "Incomprehensibilities";
 
-  printf("Comparar Arbol y Artesania: %d\n", ft_strncmp(s1, s2, 2)); //Debe ser igual a 0
-  printf("Comparar Arbol y Artesania: %d\n", ft_strncmp(s2, s1, 6)); // Debe ser -1
-  printf("Comparar Arbol y Zorro: %d\n", ft_strncmp(s1, s3, 10)); // Debe ser -1
-  printf("Comparar Zorro y Arbol: %d\n", ft_strncmp(s3, s1, 10)); // Debe ser 1
-  
-   return (0);
+  printf("TEST 1:\n");
+  printf("EXPECTED: Bed VS Bedroom, first 3 letters: \n%i\n", strncmp(s1, s2, 3));
+  printf("ACTUAL RESULT:\n%i\n\n", ft_strncmp(s1, s2, 3));
+  printf("TEST 2:\n");
+  printf("EXPECTED: Bed VS Bedroom, all letters:\n%d\n", strncmp(s1, s2, 7));
+  printf("ACTUAL RESULT:\n %d\n\n", ft_strncmp(s1, s2, 7));
+  printf("TEST 3:\n");
+  printf("EXPECTED: Bed VS Door: \n %d\n", strncmp(s1, s3, 10));
+  printf("ACTUAL RESULT: \n %d\n\n", ft_strncmp(s1, s3, 10));
+  printf("TEST 4:\n");
+  printf("EXPECTED: Door VS Bed: \n %d\n", strncmp(s3, s1, 10));
+  printf("ACTUAL RESULT: \n %d\n\n", ft_strncmp(s3, s1, 10));
+  printf("TEST 5:\n");
+  printf("EXPECTED: "" VS Incomprehensibilities: \n %d\n", strncmp(s4, s5, 10));
+  printf("ACTUAL RESULT: \n %d\n\n", ft_strncmp(s4, s5, 10));
+	return (0);
 }
