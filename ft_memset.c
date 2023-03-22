b77f4bcb71b6cf01f6ce593704882f3d265b6016
Laura Daniela <Laura@Wanaldinos-MacBook-Pro.local>
2023-03-22 19:27:58 +0100

    Norme corrections on various functions

--- ft_memset.c
+++ ft_memset.c
@@ -10,8 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
