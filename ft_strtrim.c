4bbea77c698ffdcfbeb0675c224cdf36ad351405
Laura Daniela <Laura@Wanaldinos-MacBook-Pro.local>
2023-04-13 11:24:20 +0200

    Fixed some issues and added brief

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -9,28 +9,35 @@
 /*   Updated: 2023/03/11 09:20:44 by laumoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/**
+ * @brief Trims the specified set of characters from the beginning and end
+ * of the input string and returns a new string.
+ * 
+ * @param s1 The string to be trimmed.
+ * @param set The set of characters to be trimmed from the string.
+ * 
+ * @return The trimmed string. NULL if memory allocation fails.
+ */
 
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	char	*str;
 	size_t	start;
 	size_t	end;
-	size_t	len;
-	char	*str;
 
 	if (!s1 || !set)
 		return (NULL);
 	start = 0;
-	end = ft_strlen(s1) - 1;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	while (end > start && ft_strchr(set, s1[end]))
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
 		end--;
-	len = end - start + 1;
-	str = (char *)malloc((len + 1) * sizeof(char));
+	str = (char *)malloc((end - start + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s1 + start, len + 1);
+	ft_strlcpy(str, s1 + start, end - start + 1);
 	return (str);
 }
