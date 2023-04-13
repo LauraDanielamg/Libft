27960fce0e386c1b668500c5f1552a3d043a7cd7
Laura Daniela <Laura@Wanaldinos-MacBook-Pro.local>
2023-04-13 10:51:44 +0200

    Fixed some issues and added brief

--- ft_substr.c
+++ ft_substr.c
@@ -9,38 +9,38 @@
 /*   Updated: 2023/03/11 09:10:34 by laumoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/**
+ * @brief Allocates memory for and returns a substring of a given string.
+ * 
+ * @param s Pointer to the string to create a substring from. 
+ * @param start The starting index of the substring in the original string.  
+ * @param len The maxinum leght of the substring
+ * 
+ * @return If the memory allocation is successful and the substring is created, 
+ * returns a pointer to the first character of the substring. Otherwise, returns
+ * a NULL pointer.
+ */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    size_t str_len;
-    char *substr;
+	size_t	str_len;
+	char	*substr;
 
-    // Obtén la longitud de la cadena "s".
-    str_len = ft_strlen(s);
-
-    // Si el índice de inicio es mayor o igual a la longitud de la cadena, devuelve una cadena vacía.
-    if (start >= str_len)
-    {
-        substr = malloc(1); // Reserva memoria para una cadena vacía.
-        *substr = '\0'; // Establece el primer caracter de la cadena vacía como el carácter nulo.
-        return (substr);
-    }
-
-    // Determina la longitud de la subcadena.
-    if (len > (str_len - start))
-        len = str_len - start;
-
-    // Reserva memoria para la subcadena.
-    substr = malloc((len + 1) * sizeof(char)); // La subcadena más el carácter nulo.
-
-    if (!substr)
-        return (NULL); // Si la reserva de memoria falla, devuelve NULL.
-
-    // Copia la subcadena desde la cadena "s" a la cadena "substr".
-    ft_strlcpy(substr, s + start, len);
-    substr[len] = '\0'; // Asegúrate de que la subcadena termine con un carácter nulo.
-
-    return (substr);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+	{
+		substr = malloc(1);
+		*substr = '\0';
+		return (substr);
+	}
+	if (len > (str_len - start))
+		len = str_len - start;
+	len++;
+	substr = malloc((len) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, s + start, len);
+	return (substr);
 }
