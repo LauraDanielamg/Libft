fcb75435087f4a3e1cb1c54482edf0e36d7f82f5
Laura Daniela Morales Gutierrez <laumoral@c1r1s1.42madrid.com>
2023-04-22 12:11:12 +0200

    Added test in a commentary

--- ft_itoa.c
+++ ft_itoa.c
@@ -69,3 +69,26 @@ char	*ft_itoa(int n)
 	}
 	return (strnum);
 }
+/*
+#include <stdio.h>
+int main(void)
+{
+    int num1 = 12345;
+    int num2 = -6789;
+    int num3 = 0;
+    char *str1, *str2, *str3;
+
+    str1 = ft_itoa(num1);
+    str2 = ft_itoa(num2);
+    str3 = ft_itoa(num3);
+
+    printf("Number: %d, String: %s\n", num1, str1);
+    printf("Number: %d, String: %s\n", num2, str2);
+    printf("Number: %d, String: %s\n", num3, str3);
+
+    free(str1);
+    free(str2);
+    free(str3);
+
+    return 0;
+}*/
