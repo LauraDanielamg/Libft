d07c4aa916005b4aac766c045421a8add5cdf698
Laura Daniela <Laura@Wanaldinos-MacBook-Pro.local>
2023-04-22 10:39:14 +0200

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -12,60 +12,94 @@
 
 #include "libft.h"
 
-char **ft_split(char const *s, char c)
+static char	**free_split(char **arr)
 {
-    if (!s) // Verificar si la cadena de entrada es nula
-        return NULL;
-    
-    char **result = (char **)malloc(sizeof(char *) * 128); // Reservar memoria para un array de punteros de tamaño inicial de 128
-    if (!result) // Verificar si se pudo reservar memoria
-        return NULL;
-    
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    while (s[i] != '\0') {
-        if (s[i] == c) { // Encontrar el carácter delimitador
-            if (k > 0) { // Agregar la subcadena anterior al array si no está vacía
-                result[j] = (char *)malloc(sizeof(char) * (k + 1));
-                if (!result[j]) { // Verificar si se pudo reservar memoria
-                    j--; // Retroceder para sobrescribir el último puntero
-                    while (j >= 0) {
-                        free(result[j]);
-                        j--;
-                    }
-                    free(result);
-                    return NULL;
-                }
-                result[j][k] = '\0'; // Agregar el carácter nulo al final de la subcadena
-                k = 0; // Reiniciar el contador de caracteres
-                j++; // Incrementar el índice del array
-            }
-        }
-        else { // Agregar el carácter actual a la subcadena
-            if (k == 0)
-                result[j] = NULL; // Inicializar el puntero a NULL para el primer carácter de la primera subcadena
-            k++;
-        }
-        i++; // Incrementar el índice de la cadena de entrada
-    }
-    
-    if (k > 0) { // Agregar la última subcadena al array si no está vacía
-        result[j] = (char *)malloc(sizeof(char) * (k + 1));
-        if (!result[j]) { // Verificar si se pudo reservar memoria
-            j--; // Retroceder para sobrescribir el último puntero
-            while (j >= 0) {
-                free(result[j]);
-                j--;
-            }
-            free(result);
-            return NULL;
-        }
-        result[j][k] = '\0'; // Agregar el carácter nulo al final de la última subcadena
-        j++; // Incrementar el índice del array
-    }
-    
-    result[j] = NULL; // Agregar un puntero NULL al final del array
-    
-    return result;
+	size_t	i;
+
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	while (arr[i] != NULL)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+	return (NULL);
+}
+
+static size_t	count_elements(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (!s[i])
+			break ;
+		count++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (count);
+}
+
+static char	**init_split(char const *s, char c)
+{
+	char	**arr;
+	size_t	len;
+
+	if (s == NULL)
+		return (NULL);
+	len = count_elements(s, c);
+	arr = malloc(sizeof(char *) * (len + 1));
+	if (arr == NULL)
+		return (NULL);
+	arr[len] = NULL;
+	return (arr);
+}
+
+/**
+ * @brief This function splits a string into an array of substrings based
+ *  on a delimiter character.
+ * 
+ * @param s: The string to split.
+ * @param c: The delimiter character.
+ * 
+ * @return The resulting array of new strings.
+ * NULL if the memory allocation fails.
+ */
+char	**ft_split(char const *s, char c)
+{
+	char	**arr;
+	size_t	index;
+	size_t	i;
+	size_t	start;
+
+	arr = init_split(s, c);
+	if (arr == NULL)
+		return (NULL);
+	index = 0;
+	i = 0;
+	start = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (!s[i])
+			break ;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		arr[index++] = ft_substr(s, start, i - start);
+		if (arr[index - 1] == NULL)
+			return (free_split(arr));
+	}
+	return (arr);
 }
