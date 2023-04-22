3e02ec880071d72179b1c3d628b95c1252a35241
Laura Daniela Morales Gutierrez <laumoral@c1r1s1.42madrid.com>
2023-04-22 14:09:07 +0200

    Added test in a commentary

--- ft_striteri.c
+++ ft_striteri.c
@@ -34,3 +34,50 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		}
 	}
 }
+
+/*#include <stdio.h>
+
+// Include the ft_striteri function here
+
+// Example function to be passed to ft_striteri
+void print_char_with_index(unsigned int index, char *c)
+{
+    printf("Character at index %u: %c\n", index, *c);
+}
+
+// Another example function to be passed to ft_striteri
+void to_uppercase(unsigned int index, char *c)
+{
+    if (*c >= 'a' && *c <= 'z')
+    {
+        *c = *c - 'a' + 'A';
+    }
+}
+
+int main()
+{
+    // Test case 1: Print each character along with its index
+    char test_string1[] = "Hello, World!";
+    printf("Test 1: Print each character along with its index\n");
+    ft_striteri(test_string1, print_char_with_index);
+    printf("\n");
+
+    // Test case 2: Convert the string to uppercase
+    char test_string2[] = "Hello, World!";
+    printf("Test 2: Convert the string to uppercase\n");
+    ft_striteri(test_string2, to_uppercase);
+    printf("Modified string: %s\n", test_string2);
+    printf("\n");
+
+    // Test case 3: Pass NULL as the string
+    printf("Test 3: Pass NULL as the string\n");
+    ft_striteri(NULL, print_char_with_index);
+    printf("\n");
+
+    // Test case 4: Pass NULL as the function
+    printf("Test 4: Pass NULL as the function\n");
+    ft_striteri(test_string1, NULL);
+    printf("\n");
+
+    return 0;
+}*/
